7d79e7224a2040e6b8283975588faad041c56e2a
Gustavo2709 <macedo.gustavo@uol.com.br>
2020-11-25 13:17:09 -0300

    Update Alternativa.hs
    Criando formulário

--- ShurikensFlamejantes/src/Handler/Alternativa.hs
+++ ShurikensFlamejantes/src/Handler/Alternativa.hs
@@ -12,7 +12,15 @@ import Database.Persist.Postgresql
 import Text.Lucius
 import Text.Julius
 
+-- Puxa as perguntas 
 perguntaCB = do
   rows <- runDB $ selectList [] [Asc PerguntaDescricao]
   optionsPairs $ 
       map (\r -> (perguntaDescricao $ entityVal r, entityKey r)) rows
+
+-- Formulário das alternativas
+formAlternativa :: Form Alternativa 
+formAlternativa = renderBootstrap $ Alternativa
+    <$> areq (selectField perguntaCB) "Pergunta: " Nothing
+    <*> areq textField "Alternativa: " Nothing --testar
+    <*> areq boolField "É correta?: " Nothing
