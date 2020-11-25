16ab3a98fff33cccdda099f10db4e5e3d346c9df
SrNeper <45341032+SrNeper@users.noreply.github.com>
2020-11-25 11:40:34 -0300

    Home
    montando a pag inicial..

--- ShurikensFlamejantes/src/Handler/Home.hs
+++ ShurikensFlamejantes/src/Handler/Home.hs
@@ -4,26 +4,34 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE QuasiQuotes #-}
+
 module Handler.Home where
 
 import Import
+--import Network.HTTP.Types.Status
+import Text.Lucius
+import Text.Julius
+import Database.Persist.Postgresql
+  
 
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do
+        addStylesheet (StaticR css_animate_css)
+        addStylesheet (StaticR css_icomoon_css)
         addStylesheet (StaticR css_bootstrap_css)
-        [whamlet|
-            <h1>
-                SISTEMA DE PRODUTOS
+        addStylesheet (StaticR css_superfish_css)
+        addStylesheet (StaticR css_style_css)
+        addScript (StaticR js_jquery_js)
+        addScript (StaticR js_modernizr_js)
+        addScript (StaticR js_easing_js)
+        addScript (StaticR js_bootstrap_js)
+        addScript (StaticR js_waypoints_js)
+        addScript (StaticR js_stellar_js)
+        addStylesheetRemote "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300"
+        toWidget $(juliusFile "templates/julius/hoverIntent.julius")
+        toWidget $(juliusFile "templates/julius/superfish.julius")
+        toWidget $(juliusFile "templates/julius/main.julius")
+        $(whamletFile "templates/homepage.hamlet")
         
-            <img src=@{StaticR imgs_produto_jpg}>
-        
-            <ul>
-                <li>
-                    <a href=@{ProdutoR}>
-                        CADASTRO DE PRODUTOS
-                        
-                <li>
-                    <a href=@{ListaR}>
-                        LISTAGEM
-        |]
+
