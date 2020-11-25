cf6a72a7e61ce86fda7c522adbf12b113600eca5
Gustavo2709 <macedo.gustavo@uol.com.br>
2020-11-25 13:14:49 -0300

    Criando Alternativas
    Criando Alternativas

---
+++ ShurikensFlamejantes/src/Handler/Alternativa.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Alternativa where
+
+import Import
+-- import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+import Text.Lucius
+import Text.Julius
+
+perguntaCB = do
+  rows <- runDB $ selectList [] [Asc PerguntaDescricao]
+  optionsPairs $ 
+      map (\r -> (perguntaDescricao $ entityVal r, entityKey r)) rows
