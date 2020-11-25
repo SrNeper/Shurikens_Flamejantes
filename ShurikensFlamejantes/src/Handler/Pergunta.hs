4c29344722a8809e401d004aa6cd195655d9c21e
Weskley dos Santos Rocha <weskley.s.r@hotmail.com>
2020-11-25 03:32:30 -0300

    perguntas haskell
    perguntas haskell

---
+++ ShurikensFlamejantes/src/Handler/Pergunta.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE NoImplicitPrelude #-} 
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Pergunta where
+
+import Import
+import Text.Lucius
+import Text.Julius
+--import Network.HTTP.Types.Stactus
+import Database.Persist.Postgresql
+--import Database.Esqueleto
+import Data.Text (Text, concat)
