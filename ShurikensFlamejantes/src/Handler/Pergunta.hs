c8e056c3fa5ffaedbf3a800321d97dbd589a4289
Weskley dos Santos Rocha <weskley.s.r@hotmail.com>
2020-11-25 03:43:16 -0300

    rollbak
    rollbak

--- ShurikensFlamejantes/src/Handler/Pergunta.hs
+++ ShurikensFlamejantes/src/Handler/Pergunta.hs
@@ -6,7 +6,11 @@
 {-# LANGUAGE TypeFamilies #-}
 module Handler.Pergunta where
 
-
+import Import
+import Text.Lucius
+import Text.Julius
+--import Network.HTTP.Types.Stactus
+import Database.Persist.Postgresql
 --import Database.Esqueleto
 import Data.Text (Text, concat)
 
