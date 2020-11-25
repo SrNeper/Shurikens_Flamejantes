e5f0527225796ba394ee2c5edfbb38ecb5289106
SrNeper <45341032+SrNeper@users.noreply.github.com>
2020-11-25 17:00:20 -0300

    Update Home

--- ShurikensFlamejantes/src/Handler/Home.hs
+++ ShurikensFlamejantes/src/Handler/Home.hs
@@ -34,6 +34,68 @@ getHomeR = do
         toWidget $(juliusFile "templates/julius/main.julius")
         $(whamletFile "templates/homepage.hamlet")
         
+       
+getNinjaR :: Handler Html
+getNinjaR = do
+    defaultLayout $ do
+        addStylesheet (StaticR css_animate_css)
+        addStylesheet (StaticR css_icomoon_css)
+        addStylesheet (StaticR css_bootstrap_css)
+        addStylesheet (StaticR css_superfish_css)
+        addStylesheet (StaticR css_style_css)
+        addScript (StaticR js_jquery_js)
+        addScript (StaticR js_modernizr_js)
+        addScript (StaticR js_easing_js)
+        addScript (StaticR js_bootstrap_js)
+        addScript (StaticR js_waypoints_js)
+        addScript (StaticR js_stellar_js)
+        addStylesheetRemote "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300"
+        toWidget $(juliusFile "templates/julius/hoverIntent.julius")
+        toWidget $(juliusFile "templates/julius/superfish.julius")
+        toWidget $(juliusFile "templates/julius/main.julius")
+        $(whamletFile "templates/ninja.hamlet")
+
+getQuizR :: Handler Html
+getQuizR = do
+    defaultLayout $ do
+        addStylesheet (StaticR css_animate_css)
+        addStylesheet (StaticR css_icomoon_css)
+        addStylesheet (StaticR css_bootstrap_css)
+        addStylesheet (StaticR css_superfish_css)
+        addStylesheet (StaticR css_style_css)
+        addStylesheet (StaticR css_styleQuiz_css)
+        addScript (StaticR js_jquery_js)
+        addScript (StaticR js_modernizr_js)
+        addScript (StaticR js_easing_js)
+        addScript (StaticR js_bootstrap_js)
+        addScript (StaticR js_waypoints_js)
+        addScript (StaticR js_stellar_js)
+        addStylesheetRemote "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300"
+        toWidget $(juliusFile "templates/julius/hoverIntent.julius")
+        toWidget $(juliusFile "templates/julius/superfish.julius")
+        toWidget $(juliusFile "templates/julius/main.julius")
+        toWidget $(juliusFile "templates/julius/quiz.julius")
+        $(whamletFile "templates/quiz.hamlet")
+        
+getSobreR :: Handler Html
+getSobreR = do
+    defaultLayout $ do
+        addStylesheet (StaticR css_animate_css)
+        addStylesheet (StaticR css_icomoon_css)
+        addStylesheet (StaticR css_bootstrap_css)
+        addStylesheet (StaticR css_superfish_css)
+        addStylesheet (StaticR css_style_css)
+        addScript (StaticR js_jquery_js)
+        addScript (StaticR js_modernizr_js)
+        addScript (StaticR js_easing_js)
+        addScript (StaticR js_bootstrap_js)
+        addScript (StaticR js_waypoints_js)
+        addScript (StaticR js_stellar_js)
+        addStylesheetRemote "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300"
+        toWidget $(juliusFile "templates/julius/hoverIntent.julius")
+        toWidget $(juliusFile "templates/julius/superfish.julius")
+        toWidget $(juliusFile "templates/julius/main.julius")
+        $(whamletFile "templates/sobre.hamlet")
 
 
 
