3be894e77e0fd88bcfc0fd1180cbc8a3a244208b
SrNeper <45341032+SrNeper@users.noreply.github.com>
2020-11-26 06:46:08 -0300

    Agora só usuario logado pode mexer nas perguntas

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -33,8 +33,8 @@ instance Yesod App where
     isAuthorized SobreR _ = return Authorized
     isAuthorized QuizPerguntaR _ = return Authorized
     isAuthorized QuizRespostaR _ = return Authorized
-    isAuthorized PerguntaR _ = return Authorized
-    isAuthorized AlternativaR _ = return Authorized
+    --isAuthorized PerguntaR _ = return Authorized
+    --isAuthorized AlternativaR _ = return Authorized
     isAuthorized _ _ = isUsuario
 isUsuario :: Handler AuthResult
 isUsuario = do 
